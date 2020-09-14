80b5f40deb5d44a00ce6acb5d1e1118f992dadff
AYUSH KUMAR <inc.ayush29@gmail.com>
2020-09-15 00:52:24 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,9 @@
 <?php
-//including the database connection file
+
 include("config.php");
  
-//getting id of the data from url
+
 $id = $_GET['id'];
- 
-//deleting the row from table/collection
-$db->users->deleteOne(array('_id' => new MongoId($id)));
- 
-//redirecting to the display page (index.php in our case)
+$db->users->deleteOne(array('_id' => new  MongoDB\BSON\ObjectID($id)));
 header("Location:index.php");
 ?>
